4de45488c9dbc2348886d10ec2282e46e49aac10
Chinmay Narolia <chinmay91797@gmail.com>
2021-01-17 10:24:31 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,12 +1,15 @@
+# Import pickle and Article scraping modules
 import pickle
 from newspaper import Article
 
+# Defining scrap function
 def link(url):
     url_i = Article(url="%s" % (url), language='en') 
     url_i.download()
     url_i.parse()
     return url_i.text
 
+# Defining news predicting function using previous model
 def fakenews(var):
   load_model=pickle.load(open('data/model.pickle','rb'))
   prediction=load_model.predict([var])
@@ -14,6 +17,7 @@ def fakenews(var):
   dictx={"prediction": prediction[0], "probability": prob[0][1]}
   return dictx
 
+# Running in non-GUI consol
 if __name__=='__main__':
   Userinput=input("URL: ")
   para=link(Userinput)
